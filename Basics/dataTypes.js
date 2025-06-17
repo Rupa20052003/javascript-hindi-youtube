7f0c308fee440940751583e8ae41a582b954e8fc
Rupa20052003 <hankarerupali6@gmail.com>
2025-06-17 12:50:33 +0000

    container config done

---
+++ Basics/dataTypes.js
@@ -0,0 +1,30 @@
+// "use strict" // treat all js code as newer version
+
+// // alert(3+3);  //we are using node js not a browser
+
+// console.log(3+3);  console.log("Rupali")  //Not a good it should decrease code readability
+
+console.log(2
++2);  //code readability should high
+  console.log("Hankare");
+
+
+let age =18;
+let isLoggedin=false;
+let  state;
+
+// DataTypes js
+
+// Numbers => 2 to power 53
+// bigint - for big value of number
+// string => either in '' or ""
+//boolean => it give either true or false 
+//null => standalone value
+//undefined => value not defined 
+//symbol => unique value
+
+//object
+
+console.log(typeof (6));
+console.log(typeof undefined); //o/p=undefined
+console.log(typeof null); //Object
---
+++ Basics/variable.js
@@ -0,0 +1,22 @@
+const accountId= 24567;
+let accountEmail= "rupali@gmail.com";
+var accountName="Rupali";
+accoutPassword="1235";
+accoutCity="Jaipur";
+let accountState;
+
+
+// accountId=2;  //Not allowed because it cannot change it is a const type
+accountEmail= "Hankare@gmail.com";
+accoutPassword="4656";
+accoutCity="vijapur";
+accountName="Hankare";
+accoutPassword="1235";
+console.table([accountEmail,accountName,accoutCity,accoutPassword,accountId,accountState]);
+
+/*
+Prefer not to use Var keyword in javascript
+because of issue in block scope and functional scope
+ */
+
+
