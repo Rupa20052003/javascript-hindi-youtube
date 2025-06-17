0fa024d6b377aaa11e3f034ce308d7993c189ee7
Rupa20052003 <hankarerupali6@gmail.com>
2025-06-17 13:40:51 +0000

    conversion operation

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score ="undefined";
+ let score1=null;
+console.log(typeof score);
+console.log(typeof (score1));
+
+let valueinNumber =Number(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+//note
+// when we convert "33" into number it will convert 33
+//if we convert "33abc" => not converting that id NaN
+//true => 1 and false =>0
+
+let isLoggedin=0;
+
+let booleanisLoggedin=Boolean(isLoggedin);
+console.log(booleanisLoggedin);
+
+//notes
+//when we add 1=> it gives true; and when 0=> false;
+// ""=> false
+//"rupali " => true
+
+let someNumber =33;
+
+let stringNumber =String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
