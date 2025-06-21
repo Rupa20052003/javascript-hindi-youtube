26b8d409ad29a7448395cb86425a39ca36caa42c
Rupa20052003 <hankarerupali6@gmail.com>
2025-06-21 12:59:26 +0000

    conversionOperation

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -27,3 +27,45 @@ let someNumber =33;
 let stringNumber =String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// **********************************OPERATIONS**********************************************
+
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello";
+let str2="rupali";
+
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log(1 + "2");
+// console.log("1" + "2" +3);
+ console.log((3+4) * 5%3);
+console.log(+true);
+console.log (+""); // no sense
+
+let  num1 ,num2 ,num3;
+num1 =num2=num3=2+1;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+// console.log("2" > 1);
+// console.log("02" >1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined ==0);
+// console.log(undefined>0);   //falls in  dificulty
